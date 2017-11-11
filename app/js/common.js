0831ab111dec3f838870720401d08c11307e7500
Acromyon-ПК\Acromyon <doc.domrachev@mail.ru>
2017-11-11 11:23:11 +0300

    1.4 - add eslint. optimize HTML-structure.

--- app/js/common.js
+++ app/js/common.js
@@ -1,17 +1,19 @@
-"use strict";
+'use strict';
 
-let alignItems = document.getElementsByClassName("height-align");
+const alignItems = document.getElementsByClassName('height-align');
 
 let maxItemlHeight = 0;
 
-for (let i = 0; i < alignItems.length; i++){
-	if (maxItemlHeight < alignItems[i].clientHeight){
+for (let i = 0; i < alignItems.length; i++) {
+	if (maxItemlHeight < alignItems[i].clientHeight) {
 		maxItemlHeight = alignItems[i].clientHeight;
 	}
 }
 
-for (let i = 0; i < alignItems.length; i++){
-	alignItems[i].style.height = maxItemlHeight + 'px';
+maxItemlHeight += 'px';
+
+for (let i = 0; i < alignItems.length; i++) {
+	alignItems[i].style.height = maxItemlHeight;
 }
 
 console.log(alignItems);
