b322905866f36a9967e0cc8e9269bad93969a9d6
Acromyon-ПК\Acromyon <doc.domrachev@mail.ru>
2017-11-12 10:55:28 +0300

    1.5 - add babel & uglifijs.

---
+++ app/js/common-es6.js
@@ -0,0 +1,17 @@
+const alignItems = document.getElementsByClassName('height-align');
+
+let maxItemlHeight = 0;
+
+for (let i = 0; i < alignItems.length; i++) {
+	if (maxItemlHeight < alignItems[i].clientHeight) {
+		maxItemlHeight = alignItems[i].clientHeight;
+	}
+}
+
+maxItemlHeight += 'px';
+
+for (let i = 0; i < alignItems.length; i++) {
+	alignItems[i].style.height = maxItemlHeight;
+}
+
+console.log(alignItems);
--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,32 @@ var gulp = require('gulp'),
 	cssnano = require('gulp-cssnano'),
 	rename = require('gulp-rename'),
 	browserSync = require('browser-sync'),
-	autoprefixer = require('gulp-autoprefixer');
+	autoprefixer = require('gulp-autoprefixer'),
+	babel = require('gulp-babel'),
+	uglify = require('gulp-uglifyjs');
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
 	return gulp.src('app/sass/*.sass')
-	.pipe(sass())
-	.pipe(autoprefixer(['last 15 versions', '> 1%']))
-	.pipe(concat('main.css'))
-	.pipe(gulp.dest('app/css'))
-	.pipe(cssnano())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.reload({stream: true}));
+		.pipe(sass())
+		.pipe(autoprefixer(['last 15 versions', '> 1%']))
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest('app/css'))
+		.pipe(cssnano())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('babel', function () {
+	gulp.src('app/js/common-es6.js')
+		.pipe(babel({ presets: ['env'] }))
+		.pipe(uglify())
+		.pipe(rename('common.min.js'))
+		.pipe(gulp.dest('app/js/'))
+		.pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('browser-sync', function () {
 	browserSync({
 		server: {
 			baseDir: 'app'
@@ -29,8 +40,8 @@ gulp.task('browser-sync', function() {
 
 /* Далее идут консольные таски! */
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
+gulp.task('watch', ['browser-sync', 'sass'], function () {
 	gulp.watch('app/sass/*.sass', ['sass']);
 	gulp.watch('app/*.html', browserSync.reload);
-	gulp.watch('app/js/*.js', browserSync.reload);
+	gulp.watch('app/js/*.js', ['babel']);
 });
